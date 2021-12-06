acbcf5a98408ec9af2004502c369766803d1ca4c
Tiim <tim.bachmann96@gmail.com>
2021-12-06 11:08:58 +0100

    Change constructor, add agegroup map

--- src/lenex.ts
+++ src/lenex.ts
@@ -1,14 +1,17 @@
-import { courses } from './constants.js';
-import { parseLenex } from './lenex-parse.js'
-import { LenexRaw, MeetAthlete, MeetClub } from './lenex-type.js';
+import { courses } from "./constants.js";
+import { parseLenex } from "./lenex-parse.js";
+import {
+  EventAgeGroup,
+  LenexRaw,
+  MeetAthlete,
+  MeetClub,
+} from "./lenex-type.js";
 
-type AcceptedInputTypes = Blob|Buffer|Uint8Array;
-
+type AcceptedInputTypes = Blob | Buffer | Uint8Array;
 
 export class Lenex {
-
   readonly accepted_lenex_versions = [3, "3.0"];
-  
+
   private file?: AcceptedInputTypes;
   /**
    * The raw date from the lenex file
@@ -19,6 +22,7 @@ export class Lenex {
    * Maps event-ids to events
    */
   private eventsMap: Record<string, Event>;
+  
   /**
    * Maps athlete-ids to athletes
    */
@@ -30,81 +34,111 @@ export class Lenex {
   /**
    * Maps club-codes to clubs
    */
-  private clubsMapCode: Record<string, any>;
-  
-  constructor(file: AcceptedInputTypes) {      
+  private clubsMapCode: Record<string, MeetClub>;
+
+  /**
+   * Maps age-group ids to age groups
+   */
+  private ageGroupMap: Record<string, EventAgeGroup>;
+
+  private constructor(file: AcceptedInputTypes) {
     this.file = file;
     this.raw = null;
     this.athleteMap = {};
     this.eventsMap = {};
     this.clubsMap = {};
-    this.clubsMapCode = {}
+    this.clubsMapCode = {};
   }
 
-
-  
+  static async LoadLenexFile(file: AcceptedInputTypes): Promise<Omit<Lenex, "init"| "file">> {
+    const lenex = new Lenex(file);
+    await lenex.init();
+    return lenex;
+  }
 
   async init() {
-
-
     this.raw = await parseLenex(this.file);
     delete this.file;
 
     if (!this.accepted_lenex_versions.includes(this.raw.version)) {
-      throw new Error(`Unsupported lenex version: ${this.raw.version} Supported versions: (${this.accepted_lenex_versions.join(", ")})`);
+      throw new Error(
+        `Unsupported lenex version: ${
+          this.raw.version
+        } Supported versions: (${this.accepted_lenex_versions.join(", ")})`
+      );
     }
 
+    this.eventsMap =
+      this.raw.meets
+        ?.flatMap((meet) => meet.sessions)
+        .flatMap((session) => session.events)
+        .reduce((prev, cur) => ({ ...prev, [cur.eventid]: cur }), {}) ?? {};
 
-    this.eventsMap = this.raw.meets
-      ?.flatMap(meet => meet.sessions)
-      .flatMap(session => session.events)
-      .reduce((prev, cur) => ({...prev, [cur.eventid]: cur}), {})?? {};
-    
-    this.athleteMap = this.raw.meets
-      ?.flatMap(meet => meet.clubs)
-      .filter(notNull)
-      .flatMap(club => club.athletes?.map(athlete => 
-        ({...athlete, club}))?? [])
-      .reduce((prev, cur) => ({...prev, [cur.athleteid]: cur}), {})?? {};
-      
-    this.clubsMap = this.raw.meets
-      ?.flatMap(meet => meet.clubs)
-      .filter(notNull)
-      .reduce((prev, cur) => ({...prev, [cur.clubid]: cur}), {})?? {};
-    
-    this.clubsMapCode = this.raw.meets
-      ?.flatMap(meet => meet.clubs)
-      .filter(notNull)
-      .reduce((prev, cur) => ({...prev, [cur.code]: cur}), {}) ?? {};
+    this.athleteMap =
+      this.raw.meets
+        ?.flatMap((meet) => meet.clubs)
+        .filter(notNull)
+        .flatMap(
+          (club) =>
+            club.athletes?.map((athlete) => ({ ...athlete, club })) ?? []
+        )
+        .reduce((prev, cur) => ({ ...prev, [cur.athleteid]: cur }), {}) ?? {};
+
+    this.clubsMap =
+      this.raw.meets
+        ?.flatMap((meet) => meet.clubs)
+        .filter(notNull)
+        .reduce((prev, cur) => ({ ...prev, [cur.clubid]: cur }), {}) ?? {};
+
+    this.clubsMapCode =
+      this.raw.meets
+        ?.flatMap((meet) => meet.clubs)
+        .filter(notNull)
+        .reduce((prev, cur) => ({ ...prev, [cur.code]: cur }), {}) ?? {};
+
+    this.ageGroupMap =
+      this.raw.meets
+        ?.flatMap((m) => m.sessions)
+        .flatMap((s) => s.events)
+        .flatMap((e) => e.agegroups)
+        .reduce((prev, a) => ({ ...prev, [a.agegroupid]: a }), {}) ?? {};
   }
 
   get rawLenexData(): LenexRaw {
     return JSON.parse(JSON.stringify(this.raw));
   }
 
-  getEventById(id:string) {
+  getEventById(id: string) {
     return this.eventsMap[id];
   }
 
-  getAthleteById(id:string) {
+  getAthleteById(id: string) {
     return this.athleteMap[id];
   }
 
-  getClubById(id:string) {
+  getAgeGroupById(id: string) {
+    return this.ageGroupMap[id];
+  }
+
+  getClubById(id: string) {
     return this.clubsMap[id];
   }
 
-  getClubByClubCode(clubCode:string) {
+  getClubByClubCode(clubCode: string) {
     return this.clubsMapCode[clubCode];
   }
 
   get constructorInfo() {
-    return this.raw.constructor
+    return this.raw.constructor;
   }
 
   get meetCourse() {
     const c = this.raw.meets[0].course;
-    return {type: c, ...courses[c]}
+    return { type: c, ...courses[c] };
+  }
+
+  get clubCodes() {
+    return Object.keys(this.clubsMapCode);
   }
 }
 
--- test/lenex-load.ts
+++ test/lenex-load.ts
@@ -2,17 +2,13 @@ import assert from "assert";
 import { readFile } from "fs/promises";
 import { Lenex } from "../src/index.js";
 
-
-
-describe("lenex", ()=> {
+describe("lenex", () => {
   it("should load lenex file", async () => {
     const code = "STKP";
 
     const f = await readFile("./examples/STKP_BirsPlausch_Meldungen.lxf");
 
-    const len = new Lenex(f);
-
-    await len.init();
+    const len = await Lenex.LoadLenexFile(f);
 
     const club = len.rawLenexData.meets?.map((m) => {
       const club = m.clubs?.find((c) => (c.code = code));
@@ -20,15 +16,27 @@ describe("lenex", ()=> {
     })[0];
 
     assert.notEqual(club, undefined);
-  }) 
+  });
 
   it("should collapse collections", async () => {
     const f = await readFile("./examples/STKP_BirsPlausch_Meldungen.lxf");
-    const len = new Lenex(f);
-    await len.init();
+    const len = await Lenex.LoadLenexFile(f);
 
     assert.equal(Array.isArray(len.rawLenexData.meets), true);
     const athlete = len.rawLenexData.meets[0].clubs[0].athletes[0];
-    assert.equal(Array.isArray(athlete.entries), true )
-  })
-})
+    assert.equal(Array.isArray(athlete.entries), true);
+  });
+
+  it("should group all age groups", async () => {
+    const f = await readFile("./examples/Meet-Ausschreibung.lef");
+    const len = await Lenex.LoadLenexFile(f);
+
+    const ag = len.getAgeGroupById("1065");
+
+    assert.deepEqual(ag, {
+      agegroupid: 1065,
+      agemax: 10,
+      agemin: 10,
+    });
+  });
+});
